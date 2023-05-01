9f9045d9630e674a7fe1ce32286aebbf2f65f8f2
Erik S. Carlsten <ecarlste@gmail.com>
2023-05-01 13:45:11 -0600

    Tailwind finally working

--- apps/client/pages/index.tsx
+++ apps/client/pages/index.tsx
@@ -1,8 +1,9 @@
+import { PostsCreate } from 'ui/PostsCreate';
+
 export default function Home() {
   return (
     <>
-      <h3 className="text-5xl bg-slate-600 text-white">Some font</h3>
-      <h3 className="text-xl">Some font</h3>
+      <PostsCreate />
     </>
   );
 }
--- apps/client/tailwind.config.js
+++ apps/client/tailwind.config.js
@@ -1,11 +1,1 @@
-module.exports = {
-  content: [
-    '../../packages/ui/**/*.{js,ts,jsx,tsx}',
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+module.exports = require('config/tailwind.config');
