99f6795e5cd0f4c59331084dd6b5a1baf86441a8
Erik S. Carlsten <ecarlste@gmail.com>
2023-05-01 14:17:06 -0600

    Created label and input components

--- packages/ui/PostsCreate.tsx
+++ packages/ui/PostsCreate.tsx
@@ -1,12 +1,15 @@
+import { Input } from './input';
+import { Label } from './label';
+
 export function PostsCreate() {
   return (
     <div>
       <form>
         <div>
-          <label>Title</label>
-          <input />
-          <label className="text-3xl">Title</label>
-          <input />
+          <Label>Title</Label>
+          <Input />
+          <Label>Title</Label>
+          <Input />
         </div>
         <button>Submit</button>
       </form>
--- packages/ui/tailwind.config.js
+++ packages/ui/tailwind.config.js
@@ -1,1 +1,1 @@
-module.exports = require('@ecarlste/configs/tailwind/tailwind.config');
+module.exports = require('config/tailwind.config');
