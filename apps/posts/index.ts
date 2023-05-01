f9f6d9a4fbd014684b7ba6b144a3e25e3703d526
Erik S. Carlsten <ecarlste@gmail.com>
2023-05-01 08:56:02 -0600

    Got very simple GET and POST /posts working

--- apps/posts/index.ts
+++ apps/posts/index.ts
@@ -1,16 +1,29 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import { v4 as uuidv4 } from 'uuid';
 
 const app = express();
+app.use(bodyParser.json());
+
 const port = 4000;
 
+interface post {
+  id: string;
+  title: string;
+}
+
+const posts: { [key: string]: post } = {};
+
 app.get('/posts', (req, res) => {
-  console.log('GET /posts');
-  res.status(200).send('GET /posts');
+  res.status(200).send(posts);
 });
 
 app.post('/posts', (req, res) => {
-  console.log('POST /posts');
-  res.status(200).send('POST /posts');
+  const createdPost = { id: uuidv4(), title: req.body.title };
+
+  posts[createdPost.id] = createdPost;
+
+  res.status(201).send(createdPost);
 });
 
 app.listen(port, () => {
