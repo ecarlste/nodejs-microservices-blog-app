fe15af4c10aba2f26f59458ba733786e4e19d6a1
Erik S. Carlsten <ecarlste@gmail.com>
2023-05-01 08:31:58 -0600

    Set up initial running post service express app

--- apps/posts/index.ts
+++ apps/posts/index.ts
@@ -1,1 +1,18 @@
-console.log('this is my boomstick!!!');
+import express from 'express';
+
+const app = express();
+const port = 4000;
+
+app.get('/posts', (req, res) => {
+  console.log('GET /posts');
+  res.status(200).send('GET /posts');
+});
+
+app.post('/posts', (req, res) => {
+  console.log('POST /posts');
+  res.status(200).send('POST /posts');
+});
+
+app.listen(port, () => {
+  console.log(`Posts Service running at: http://localhost:${port}`);
+});
